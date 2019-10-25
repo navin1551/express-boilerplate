4f247c6044aa2c1fc14c63da5e9b7cabc790109f
Navin Kumar <navin1551@gmail.com>
2019-10-25 15:25:46 -0700

    fixed order of helmet and cors

--- src/app.js
+++ src/app.js
@@ -10,8 +10,8 @@ const app = express();
 const morganOption = NODE_ENV === "production" ? "tiny" : "common";
 
 app.use(morgan(morganOption));
-app.use(cors());
 app.use(helmet());
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Hello, world!");
